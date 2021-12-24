57e9606caa02473793569c697515728228837bd5
Mihika Singh <mihikasingh1501@gmail.com>
2021-12-24 14:55:04 +0530

    first

---
+++ index.js
@@ -0,0 +1,24 @@
+ var randomNumber1=Math.random()*6;
+randomNumber1=Math.floor(randomNumber1)+1;
+ var randomImageDice="dice"+randomNumber1+".png";
+ var randomImageSource="images/"+randomImageDice;
+ var image=document.querySelectorAll("img")[0];
+  image.setAttribute("src",randomImageSource);
+  var randomNumber2=Math.random()*6;
+randomNumber2=Math.floor(randomNumber2)+1;
+ 
+ var randomImageSource1="images/dice" + randomNumber2 + ".png";
+  
+ document.querySelectorAll("img")[1].setAttribute("src",randomImageSource1);;
+ 
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="player1 wins";
+}
+else if(randomNumber2>randomNumber1){
+     document.querySelector("h1").innerHTML="player2 wins";
+}
+ else{
+     
+         document.querySelector("h1").innerHTML="DRAW";
+    }
+ 
